503832ff2894cfe04a4847dcadfa390224ab109f
Arno V <118028859+arnos-stuff@users.noreply.github.com>
2023-01-04 17:54:39 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,10 +2,14 @@
 $themes = Split-Path $(pshazz which linux)
 
 echo "Installing themes to $themes"
+echo "This will overwrite existing themes with the same name"
 
 $arr = $(ls *.json)
 
 foreach($thm in $arr) {
+	if (Test-Path -Path $file -PathType Leaf) {
+		rm $themes/$thm
+	}
 	$thm = Split-Path $thm -leaf
 	Write-Progress -Activity "Moving: $thm ==> $themes/$thm"
 	mv $thm $themes/$thm
